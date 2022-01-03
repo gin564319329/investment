f2adbc4422f616aeed4a4266cbf27354649d07aa
gin564319329 <chlgch_2006@163.com>
2022-01-03 18:29:27 +0800

    d

--- advance_fun.py
+++ advance_fun.py
@@ -1,164 +1,113 @@
-# -*-coding:utf-8-*-
-
-from datetime import datetime, timedelta
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import logging
 from get_market_data import GetTuShareData
-from fund_tools import CalFixedInvest, CalYieldRate, CalTime
-from show_rst import ShowRst
-
-import requests
-from bs4 import BeautifulSoup
-import re
-import matplotlib
-import tushare as ts
-from IPython.display import display
+from fund_tools import CalFixedInvest, CalYieldRate
 
 
-get_data = GetTuShareData()
-cal_base = CalYieldRate()
+class AdvOperation(GetTuShareData):
 
+    def __init__(self, cal_tools):
+        super(AdvOperation, self).__init__()
+        self.cal_base = cal_tools
 
-def get_daily_data(ts_code, date_start, date_end):
-    tu_data = get_data.get_index_daily(ts_code, date_start, date_end)
-    cal_data_tu = get_data.gen_cal_data(tu_data)
-    return cal_data_tu
+    def get_daily_data(self, ts_code, date_start, date_end):
+        tu_data = self.get_index_daily(ts_code, date_start, date_end)
+        cal_data_tu = self.gen_cal_data(tu_data)
+        return cal_data_tu
 
+    def search_code_batch(self, search_list):
+        df_sh = self.get_index_basic('', '', market='SSE')
+        df_sz = self.get_index_basic('', '', market='SZSE')
+        search_dict = {'ts_code': [], 'name': []}
 
-def search_code_batch(search_list):
-    df_sh = get_data.get_index_basic('', '', market='SSE')
-    df_sz = get_data.get_index_basic('', '', market='SZSE')
-    search_dict = {'ts_code': [], 'name': []}
+        for index, s in df_sh.iterrows():
+            if s['name'] in search_list:
+                search_list.remove(s['name'])
+                print('{}\t代码:\t {}'.format(s['name'], s['ts_code']))
+                search_dict['ts_code'].append(s['ts_code'])
+                search_dict['name'].append(s['name'])
 
-    for index, s in df_sh.iterrows():
-        if s['name'] in search_list:
-            search_list.remove(s['name'])
-            print('{}\t代码:\t {}'.format(s['name'], s['ts_code']))
-            search_dict['ts_code'].append(s['ts_code'])
-            search_dict['name'].append(s['name'])
+        for index, s in df_sz.iterrows():
+            if s['name'] in search_list:
+                search_list.remove(s['name'])
+                print('{}\t代码:\t {}'.format(s['name'], s['ts_code']))
+                search_dict['ts_code'].append(s['ts_code'])
+                search_dict['name'].append(s['name'])
 
-    for index, s in df_sz.iterrows():
-        if s['name'] in search_list:
-            search_list.remove(s['name'])
-            print('{}\t代码:\t {}'.format(s['name'], s['ts_code']))
-            search_dict['ts_code'].append(s['ts_code'])
-            search_dict['name'].append(s['name'])
+        return search_dict
 
-    return search_dict
+    def get_change_ratio(self, cal_data_tu):
+        st = cal_data_tu.iloc[0]['price']
+        end = cal_data_tu.iloc[-1]['price']
+        c_ratio = self.cal_base.cal_change_ratio(st, end)
+        return c_ratio
 
+    def get_invest_rst(self, cal_data_tu):
+        weekday = 4
+        fit = CalFixedInvest(cal_data_tu, money_amount=500)
+        df_invest_data_w = fit.fixed_invest_by_week(weekday=weekday)
+        principal_w, final_amount_w, profit_w, buy_num_w, pri_average_w = fit.cal_yield(df_invest_data_w)
+        i_ratio = self.cal_base.cal_change_ratio(principal_w, final_amount_w)
+        return i_ratio
 
-def get_change_ratio(cal_data_tu):
-    st = cal_data_tu.iloc[0]['price']
-    end = cal_data_tu.iloc[-1]['price']
-    c_ratio = cal_base.cal_change_ratio(st, end)
-    return c_ratio
+    def get_ratio_batch(self, date_search_dict, ts_code_list):
+        for code in ts_code_list:
+            for start, end in zip(date_search_dict['date_start'], date_search_dict['date_end']):
+                cal_data_tu = self.get_daily_data(code, start, end)
+                ch_ratio = self.get_change_ratio(cal_data_tu)
+                print('{} {} change ratio: {:.2%}'.format(cal_data_tu.iloc[-1]['year'], code, ch_ratio))
+                in_ratio = self.get_invest_rst(cal_data_tu)
+                print('{} {} irri profit: {:.2%}'.format(cal_data_tu.iloc[-1]['year'], code, in_ratio))
 
-
-def get_invest_rst(cal_data_tu):
-    weekday = 4
-    fit = CalFixedInvest(cal_data_tu, money_amount=500)
-    df_invest_data_w = fit.fixed_invest_by_week(weekday=weekday)
-    principal_w, final_amount_w, profit_w, buy_num_w, pri_average_w = fit.cal_yield(df_invest_data_w)
-    i_ratio = cal_base.cal_change_ratio(principal_w, final_amount_w)
-    return i_ratio
-
-
-def get_ratio_batch(date_search_dict, ts_code_list):
-    for code in ts_code_list:
+    def get_ratio_batch_by_date(self, date_search_dict, code_search_dict):
+        rst_dict_date = {'year': []}
+        rst_dict_ch = {'{}_涨跌幅'.format(na): [] for na in code_search_dict.get('name')}
+        rst_dict_in = {'{}_定投收益'.format(na): [] for na in code_search_dict.get('name')}
         for start, end in zip(date_search_dict['date_start'], date_search_dict['date_end']):
-            cal_data_tu = get_daily_data(code, start, end)
-            ch_ratio = get_change_ratio(cal_data_tu)
-            print('{} {} change ratio: {:.2%}'.format(cal_data_tu.iloc[-1]['year'], code, ch_ratio))
-            in_ratio = get_invest_rst(cal_data_tu)
-            print('{} {} irri profit: {:.2%}'.format(cal_data_tu.iloc[-1]['year'], code, in_ratio))
+            rst_dict_date['year'].append(end[0:4])
+            for code, name in zip(code_search_dict.get('ts_code'), code_search_dict.get('name')):
+                cal_data_tu = self.get_daily_data(code, start, end)
+                ch_ratio = self.get_change_ratio(cal_data_tu)
+                in_ratio = self.get_invest_rst(cal_data_tu)
+                rst_dict_ch['{}_涨跌幅'.format(name)].append(ch_ratio)
+                rst_dict_in['{}_定投收益'.format(name)].append(in_ratio)
+                print('{} {} change ratio: {:.2%}'.format(cal_data_tu.iloc[-1]['year'], name, ch_ratio))
+                print('{} {} irri profit: {:.2%}'.format(cal_data_tu.iloc[-1]['year'], name, in_ratio))
+        rst_dict_date['year'][-1] = 'all'
+        rst_data = pd.DataFrame(rst_dict_date)
+        rst_df_ch = pd.concat([rst_data, pd.DataFrame(rst_dict_ch)], axis=1)
+        rst_df_in = pd.concat([rst_data, pd.DataFrame(rst_dict_in)], axis=1)
+        rst_df = pd.concat([rst_data, pd.DataFrame(rst_dict_ch), pd.DataFrame(rst_dict_in)], axis=1)
+        return rst_df, rst_df_ch, rst_df_in
 
+    @staticmethod
+    def cal_contrast(rst_df_raw):
+        rst_ch_con = rst_df_raw.copy()
+        rst_ch_con['最大收益指数'] = rst_df_raw.iloc[:, 1:].idxmax(axis=1).values
+        rst_ch_con['最小收益指数'] = rst_df_raw.iloc[:, 1:].idxmin(axis=1).values
+        return rst_ch_con
 
-def get_ratio_batch_by_date(date_search_dict, code_search_dict):
-    rst_dict_date = {'year': []}
-    rst_dict_ch = {'{}_涨跌幅'.format(na): [] for na in code_search_dict.get('name')}
-    rst_dict_in = {'{}_定投收益'.format(na): [] for na in code_search_dict.get('name')}
-    for start, end in zip(date_search_dict['date_start'], date_search_dict['date_end']):
-        rst_dict_date['year'].append(end[0:4])
-        for code, name in zip(code_search_dict.get('ts_code'), code_search_dict.get('name')):
-            cal_data_tu = get_daily_data(code, start, end)
-            ch_ratio = get_change_ratio(cal_data_tu)
-            in_ratio = get_invest_rst(cal_data_tu)
-            rst_dict_ch['{}_涨跌幅'.format(name)].append(ch_ratio)
-            rst_dict_in['{}_定投收益'.format(name)].append(in_ratio)
-            print('{} {} change ratio: {:.2%}'.format(cal_data_tu.iloc[-1]['year'], name, ch_ratio))
-            print('{} {} irri profit: {:.2%}'.format(cal_data_tu.iloc[-1]['year'], name, in_ratio))
-    rst_dict_date['year'][-1] = 'all'
-    rst_data = pd.DataFrame(rst_dict_date)
-    rst_df_ch = pd.concat([rst_data, pd.DataFrame(rst_dict_ch)], axis=1)
-    rst_df_in = pd.concat([rst_data, pd.DataFrame(rst_dict_in)], axis=1)
-    rst_df = pd.concat([rst_data, pd.DataFrame(rst_dict_ch), pd.DataFrame(rst_dict_in)], axis=1)
-    return rst_df, rst_df_ch, rst_df_in
+    def get_ts_code_by_code(self, code, market='E'):
+        """查询tushare基金代码"""
+        fund_bat = self.get_fund_basic(market=market, status='L')
+        for ts_code in fund_bat['ts_code']:
+            if code in ts_code:
+                return ts_code
+        logging.warning('there is no {} fund'.format(code))
+        return None
 
-
-def cal_contrast(rst_df_raw):
-    rst_ch_con = rst_df_raw.copy()
-    rst_ch_con['最大收益指数'] = rst_df_raw.iloc[:, 1:].idxmax(axis=1).values
-    rst_ch_con['最小收益指数'] = rst_df_raw.iloc[:, 1:].idxmin(axis=1).values
-    return rst_ch_con
-
-
-def get_ts_code_by_code(code, market='E'):
-    """查询tushare基金代码"""
-    fund_bat = get_data.get_fund_basic(market=market, status='L')
-    for ts_code in fund_bat['ts_code']:
-        if code in ts_code:
-            return ts_code
-    logging.warning('there is no {} fund'.format(code))
-    return None
-
-
-def dis_pd_style():
-    pd.set_option('display.unicode.ambiguous_as_wide', True)
-    pd.set_option('display.unicode.east_asian_width', True)
-    pd.set_option('display.width', 1)
+    @staticmethod
+    def dis_pd_style():
+        pd.set_option('display.unicode.ambiguous_as_wide', True)
+        pd.set_option('display.unicode.east_asian_width', True)
+        pd.set_option('display.width', 1)
 
 
 if __name__ == '__main__':
-    # name_search_list = ['上证指数', '沪深300', '中证500', '上证50', '中证1000', '国证2000', '创业板指', '中证100']
-    name_search_list = ['上证指数', '上证50', '沪深300', '中证500', '创业板指']
-    # name_search_list = ['沪深300', '中证500']
-    search_rst = search_code_batch(name_search_list)
-    print(search_rst)
-
-    # date_start = '20201230'
-    # date_end = '20211231'
-    # # ts_code = '399300.SZ'
-    # # ts_code = '399905.SZ' #'000001.SH'  #'399905.SZ'
-    # # ts_code = '399006.SZ'
-    # ts_code = '000001.SH'
-    # cal_data_tu = get_daily_data(ts_code, date_start, date_end)
-    # print(cal_data_tu.iloc[0]['date'], cal_data_tu.iloc[-1]['date'])
-    # ch_ratio = get_change_ratio(cal_data_tu)
-    # print('{:.2%}'.format(ch_ratio))
-    # in_ratio = get_invest_rst(cal_data_tu)
-    # print('irri profit: {:.2%}'.format(in_ratio))
-
-    code_search = ['000001.SH', '000300.SH', '000905.SH']
-    date_search = dict()
-    date_search['date_start'] = ['20111230', '20121231', '20131231', '20141231', '20151231', '20161230', '20171229',
-                                 '20181228', '20191231', '20201231', '20111230']
-    date_search['date_end'] = ['20121231', '20131231', '20141231', '20151231', '20161231', '20171231', '20181231',
-                               '20191231', '20201231', '20211022', '20211022']
-    # date_search['date_start'] = ['20161230', '20171229', '20181228']
-    # date_search['date_end'] = ['20171231', '20181231', '20191231']
-    rst, rst_ch, rst_in = get_ratio_batch_by_date(date_search, search_rst)
-    rst_ch_con = cal_contrast(rst_ch)
-    rst_in_con = cal_contrast(rst_in)
-
-    rst.to_csv('D:\\project_codes\\investment\\ratio_rst.csv', index=False, encoding='utf_8_sig')
-    rst_ch_con.to_csv('D:\\project_codes\\investment\\ratio_rst_ch.csv', index=False, encoding='utf_8_sig')
-    rst_in_con.to_csv('D:\\project_codes\\investment\\ratio_rst_in.csv', index=False, encoding='utf_8_sig')
-
-    dis_pd_style()
-    print(rst)
-    print(rst_ch_con)
-    print(rst_in_con)
-
-
+    ad = AdvOperation(CalYieldRate())
+    cal_data = ad.get_daily_data('000001.SH', '20201231', '20211231')
+    print(cal_data.iloc[0]['date'], cal_data.iloc[-1]['date'])
+    ch_r = ad.get_change_ratio(cal_data)
+    print('{:.2%}'.format(ch_r))
+    in_r = ad.get_invest_rst(cal_data)
+    print('irri profit: {:.2%}'.format(in_r))
--- main_calculate.py
+++ main_calculate.py
@@ -1,44 +1,75 @@
 from get_market_data import GetTuShareData
 from fund_tools import CalFixedInvest, CalYieldRate, CalTime
 from show_rst import ShowRst
+from advance_fun import AdvOperation
 
 
-date_start = '20151231'
-date_end = '20171231'
-ts_code = '000001.SH'
-# ts_code = '000300.SH'
-# ts_code = '399905.SZ'
-weekday = 4
-m_day = 20
-tu_data = GetTuShareData().get_index_daily(ts_code, date_start, date_end)
-cal_data_tu = GetTuShareData().gen_cal_data(tu_data)
+def cal_value_ratio_batch(name_search_list):
+    ad = AdvOperation(CalYieldRate())
+    search_rst = ad.search_code_batch(name_search_list)
+    date_search = {'date_start': ['20111230', '20121231', '20131231', '20141231', '20151231', '20161230', '20171229',
+                                  '20181228', '20191231', '20201231', '20111230'],
+                   'date_end': ['20121231', '20131231', '20141231', '20151231', '20161231', '20171231', '20181231',
+                                '20191231', '20201231', '20211231', '20211231']}
+    rst, rst_ch, rst_in = ad.get_ratio_batch_by_date(date_search, search_rst)
+    rst_ch_con = ad.cal_contrast(rst_ch)
+    rst_in_con = ad.cal_contrast(rst_in)
 
-fit = CalFixedInvest(cal_data_tu, money_amount=500)
-fit_m = CalFixedInvest(cal_data_tu, money_amount=2000)
-df_invest_data_w = fit.fixed_invest_by_week(weekday=weekday)
-df_invest_data_m = fit_m.fixed_invest_by_month(month_day=m_day)
-principal_w, final_amount_w, profit_w, buy_num_w, pri_average_w = fit.cal_yield(df_invest_data_w)
-principal_m, final_amount_m, profit_m, buy_num_m, pri_average_m = fit.cal_yield(df_invest_data_m)
-print('week: principal: {}, final_amount: {}, profit: {}, buy_num: {}'.format(principal_w, final_amount_w, profit_w, buy_num_w))
-print('month: principal: {}, final_amount: {}, profit: {}, buy_num: {}'.format(principal_m, final_amount_m, profit_m, buy_num_m))
+    rst.to_csv(r'D:\project_codes\investment\rst_out\ratio_rst1.csv', index=False, encoding='utf_8_sig')
+    rst_ch_con.to_csv(r'D:\project_codes\investment\rst_out\ratio_rst_ch1.csv', index=False, encoding='utf_8_sig')
+    rst_in_con.to_csv(r'D:\project_codes\investment\rst_out\ratio_rst_in1.csv', index=False, encoding='utf_8_sig')
+
+    ad.dis_pd_style()
+    print(rst)
+    print(rst_ch_con)
+    print(rst_in_con)
 
 
-# cal_time = CalTime(date_start, date_end)
-# month_num = cal_time.cal_month_num()
-# x_ticks = cal_time.gen_ticks(month_num)
-val_arr_w = [500] * 50
-val_arr_w.append(-final_amount_w)
+if __name__ == '__main__':
+    name_search = ['上证指数', '沪深300', '中证500', '上证50', '中证1000', '国证2000', '创业板指', '中证100']
+    cal_value_ratio_batch(name_search)
 
-val_arr_m = [2000] * 12
-val_arr_m.append(-final_amount_m)
 
-yr = CalYieldRate()
-rate_w = yr.cal_total_rate(principal_w, final_amount_w)
-rate_m = yr.cal_total_rate(principal_m, final_amount_m)
-rate_w_irr = yr.cal_irr_by_fixed_invest(val_arr_w, 50)
-rate_m_irr = yr.cal_irr_by_fixed_invest(val_arr_m, 12)
 
-show_r = ShowRst()
-ax = show_r.gen_fig()
-show_r.show_cumulative_value(ax, cal_data_tu, df_invest_data_w)
-show_r.show_average_principal(ax, pri_average_w)
+
+
+
+# date_start = '20151231'
+# date_end = '20171231'
+# ts_code = '000001.SH'
+# # ts_code = '000300.SH'
+# # ts_code = '399905.SZ'
+# weekday = 4
+# m_day = 20
+# tu_data = GetTuShareData().get_index_daily(ts_code, date_start, date_end)
+# cal_data_tu = GetTuShareData().gen_cal_data(tu_data)
+#
+# fit = CalFixedInvest(cal_data_tu, money_amount=500)
+# fit_m = CalFixedInvest(cal_data_tu, money_amount=2000)
+# df_invest_data_w = fit.fixed_invest_by_week(weekday=weekday)
+# df_invest_data_m = fit_m.fixed_invest_by_month(month_day=m_day)
+# principal_w, final_amount_w, profit_w, buy_num_w, pri_average_w = fit.cal_yield(df_invest_data_w)
+# principal_m, final_amount_m, profit_m, buy_num_m, pri_average_m = fit.cal_yield(df_invest_data_m)
+# print('week: principal: {}, final_amount: {}, profit: {}, buy_num: {}'.format(principal_w, final_amount_w, profit_w, buy_num_w))
+# print('month: principal: {}, final_amount: {}, profit: {}, buy_num: {}'.format(principal_m, final_amount_m, profit_m, buy_num_m))
+#
+#
+# # cal_time = CalTime(date_start, date_end)
+# # month_num = cal_time.cal_month_num()
+# # x_ticks = cal_time.gen_ticks(month_num)
+# val_arr_w = [500] * 50
+# val_arr_w.append(-final_amount_w)
+#
+# val_arr_m = [2000] * 12
+# val_arr_m.append(-final_amount_m)
+#
+# yr = CalYieldRate()
+# rate_w = yr.cal_total_rate(principal_w, final_amount_w)
+# rate_m = yr.cal_total_rate(principal_m, final_amount_m)
+# rate_w_irr = yr.cal_irr_by_fixed_invest(val_arr_w, 50)
+# rate_m_irr = yr.cal_irr_by_fixed_invest(val_arr_m, 12)
+#
+# show_r = ShowRst()
+# ax = show_r.gen_fig()
+# show_r.show_cumulative_value(ax, cal_data_tu, df_invest_data_w)
+# show_r.show_average_principal(ax, pri_average_w)
